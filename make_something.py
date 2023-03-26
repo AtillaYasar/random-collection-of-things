a08a5c3a08044ea59c7d5c72aa0d4d78fa06a68f
Atilla Yasar <112716905+AtillaYasar@users.noreply.github.com>
2023-03-26 19:55:46 +0200

    improved defaulting
    you can now put for example, your default python file in "make_something_defaultpy.txt" and it will use that as the "custom default"
    has a fallback default "hardcoded" in default_y, and then a base_level, which is just an empty string. (which according to one esteemed philosopher is all you would need if you were smart enough)
    
    behavior is implemented in `def get_default(ext)`

--- make_something.py
+++ make_something.py
@@ -1,4 +1,3 @@
-
 import os, sys
 
 def text_append(path, appendage):
@@ -37,6 +36,19 @@ from examples.draggable_circles import DraggableCircle
 from general_utils.for_colors import col
 from general_utils.misc import Recursion, HierarchyItem, message, is_empty
 from tkinter_utils.class_extensions import MyText
+
+
+def col(ft, s):
+    """For printing text with colors.
+    
+    Uses ansi escape sequences. (ft is "first two", s is "string")"""
+    # black-30, red-31, green-32, yellow-33, blue-34, magenta-35, cyan-36, white-37
+    u = '\u001b'
+    numbers = dict([(string,30+n) for n, string in enumerate(('bl','re','gr','ye','blu','ma','cy','wh'))])
+    n = numbers[ft]
+    return f'{u}[{n}m{s}{u}[0m'
+
+
 """
 '''[1:-1]
 
@@ -51,18 +63,56 @@ default_html = '''
 </html>
 '''[1:-1]
 
+base_level = ''
+
+default_defaults = {
+    'py':default_py,
+    'html':default_html,
+}
+custom_default_files = {
+    'py':'make_something_defaultpy.txt',
+}
+#custom_default_files = {k:v for k,v in custom_default_files.items() if os.path.isfile(v) == True}  # validation
+
+def get_default(ext):
+    """First try custom_default, then default_default, then resort to base_level."""
+    
+    base_level = ''
+    if ext in custom_default_files.keys():
+        path = custom_default_files[ext]
+        if os.path.isfile(path):
+            finding = text_read(path)
+        else:
+            finding = None
+    elif ext in default_defaults[ext]:
+        finding = default_defaults[ext]
+    else:
+        finding = None
+    
+    if finding == None:
+        return base_level
+    else:
+        return finding
+
 while True:
     print('-')
-    print(col('cy', 'write a file name'))
+    for line in [
+        'to make custom default files, put them in these filenames:',
+        '-'*10,
+        '\n'.join([
+            f'extension:{k}, path:{v}' for k,v in custom_default_files.items()
+        ]),
+        '-'*10,
+        col('cy', 'write a file name'),
+    ]:
+        print(line)
+    print()
+
+    print()
     i = input()
     if i not in os.listdir():
-        ext = i.partition('.')[2]
-        if ext == 'html':
-            content = default_html
-        elif ext == 'py':
-            content = default_py
-        else:
-            content = ''
+        ext = i.split('.')[-1]
+        content = get_default(ext)
         text_create(i, content)
         print(col('gr',f'successfully created ') + f'{i}')
     else:
