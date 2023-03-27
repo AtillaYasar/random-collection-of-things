8fc402b565386c8a349ee71975a3cffd43a46730
Atilla Yasar <112716905+AtillaYasar@users.noreply.github.com>
2023-03-27 22:56:20 +0200

    improve playlist downloading

--- pytube_wrapper.py
+++ pytube_wrapper.py
@@ -1,5 +1,8 @@
 from pytube import YouTube, Playlist
+from overall_imports import*
+import json, os, sys
 
+# making something beautiful into something ugly, for the sake of convenience..
 class PytubeHandler:
     """Reducing the pytube api down to a couple simple things I want to do with it."""
 
@@ -58,7 +61,7 @@ class PytubeHandler:
                 result[k] = v
         return result
     
-    def download_720p(self, link:str):
+    def download_720p(self, link:str, folderpath=None):
         """Easy version of the full downloading capabilities."""
 
         success = False
@@ -66,17 +69,53 @@ class PytubeHandler:
         for item in downloadables:
             if item['resolution'] == '720p':
                 print(f'start downloading {item["title"]}')
-                item['stream object'].download()
+                if folderpath == None:
+                    item['stream object'].download()
+                else:
+                    item['stream object'].download(output_path = folderpath)
                 success = True
                 break
-        
+
         if success:
             print(f'download of {item["title"]} finished')
         else:
             print(f'could not get 720p for {item["title"]}')
 
+def download_playlist(handler_object, link):
+    """Uses PytubeHandler.download_720p. Makes a folder for the download."""
+
+    assert type(handler_object) == PytubeHandler
+    
+    pl_object = Playlist(link)
+    pl_title = pl_object.title
+    video_links = list(pl_object)
+
+    # make folder
+    ## tfw too low iq for recursion
+    p0 = 'downloads'
+    p1 = 'playlists'
+    p2 = "".join(x for x in pl_title if x.isalnum())  # make string usable in path
+    if p0 not in os.listdir():
+        os.mkdir(p0)
+    if p1 not in os.listdir(p0):
+        os.mkdir(f'{p0}/{p1}')
+    if p2 not in os.listdir(f'{p0}/{p1}'):
+        os.mkdir(f'{p0}/{p1}/{p2}')
+    folder = '/'.join([p0,p1,p2])
+    
+    def check_existence(string):
+        return False
+
+    for link in video_links:
+        if check_existence(link):
+            print(f'skipping {link}, already exists')
+        else:
+            handler_object.download_720p(link, folderpath=folder)
+    
+    print('completed all downloads.')
+
 myra_list = 'https://www.youtube.com/playlist?list=PLU_8MEhMgb7RHhLujJ8YdZZs3rNv6Xp6p'
 ph = PytubeHandler()
+download_playlist(ph, myra_list)
 
-to_try = Playlist(myra_list)[0]
-ph.download_720p(to_try)
+
